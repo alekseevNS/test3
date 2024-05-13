d236b6e28318bac4b525386caaeeeb942bb7c59e
Алексеев Николай Сергеевич <asakura475@gmail.com>
2024-05-13 19:42:14 +0300

    Добавил верхнюю панель

--- ajax_site/views.py
+++ ajax_site/views.py
@@ -2,8 +2,15 @@ from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
 
 
+def get_base_menu() -> list:
+    return [
+        {"link": "/", "text": "Главная"},
+        {"link": "/json_test/", "text": "Проверка получения JSON"},
+    ]
+
+
 def index_page(request: HttpRequest) -> HttpResponse:
-    context = {"page_name": "Главная"}
+    context = {"page_name": "Главная", "menu_items": get_base_menu()}
 
     return render(request, "ajax_site/index_page.html", context)
 
--- static/ajax_site/scripts/index_page.js
+++ static/ajax_site/scripts/index_page.js
@@ -6,9 +6,9 @@ function showAlert() {
 // 1. Надо получить объект кнопки
 // querySelector() -- получение ПЕРВОГО объекта DOM'а
 // Можно получать по тегу, классу, id
-let button = document.querySelector("button");
+let button = document.querySelector("#cardButton");
 // 2. Привязываем функцию выше к кнопке
 button.addEventListener("click", showAlert);
 
 // Пример от Коробова Даниила
-// document.querySelector("button").addEventListener("click", () => { alert("Hello World!") });
+// document.querySelector("#cardButton").addEventListener("click", () => { alert("Hello World!") });
